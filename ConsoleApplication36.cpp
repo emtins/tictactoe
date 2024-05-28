1926be2497de8bf5ae9a959d59a59efa2e605ae3
emtins <141028370+emtins@users.noreply.github.com>
2024-05-28 22:53:21 +0300

    Створення ігрового поля

--- ConsoleApplication36.cpp
+++ ConsoleApplication36.cpp
@@ -1,2 +1,22 @@
+#include <iostream>
+using namespace std;
 
+const int Size = 3;
+char board[Size][Size] = { {' ',' ',' '}, {' ',' ',' '}, {' ',' ',' '} };
+char player = 'X';
+
+
+void Board() {
+    cout << "  A  B  C" << endl;
+    for (int i = 0; i < Size; i++) {
+        cout << i + 1 << " ";
+        for (int j = 0; j < Size; j++) {
+            cout << board[i][j];
+            if (j < Size - 1) cout << " |";
+        }
+        cout << endl;
+        if (i < Size - 1) cout << "  --+--+--" << endl;
+    }
+    cout << endl;
+}
 
