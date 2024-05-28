7d4857a9c35193c76063fb0361c684490eb136f8
emtins <141028370+emtins@users.noreply.github.com>
2024-05-28 23:01:19 +0300

    Додавання функцій вибору поля та зображення ігрового майданчинка
    Функція Move перевіряє чи не зайняте місце куди гравець намагається поставити хрестик чи нулик і якщо місце порожне то ставить туди фігуру гравця
    
    Функція Draw перевіряє чи заповнена ігрова дошка повністю без будь-яких порожніх місць

--- ConsoleApplication36.cpp
+++ ConsoleApplication36.cpp
@@ -29,3 +29,24 @@ void Player() {
     }
 }
 
+bool Move(char col, int row) {
+    int c = col - 'A';
+    int r = row - 1;
+    if (c < 0 || c >= Size || r < 0 || r >= Size || board[r][c] == 'X' || board[r][c] == 'O') {
+        cout << "This place is taken" << endl;
+        return false;
+    }
+    board[r][c] = player;
+    return true;
+}
+
+bool Draw() {
+    for (int i = 0; i < Size; i++) {
+        for (int j = 0; j < Size; j++) {
+            if (board[i][j] != 'X' && board[i][j] != 'O') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
