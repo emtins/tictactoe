f53630e98b8f34eb8ef2a51b73fd032804c46175
emtins <141028370+emtins@users.noreply.github.com>
2024-05-29 22:17:19 +0300

    Створення прототипів; розподіл коду по файлах

---
+++ Tictactoe-Alina.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include <Windows.h>
+#include "TictactoeAlina.h"
+using namespace std;
+
+const int Size = 3;
+char board[Size][Size] = { {' ',' ',' '}, {' ',' ',' '}, {' ',' ',' '} };
+char player = 'X';
+
+
+void TicTacToe::Board() {
+    cout << "  A  B  C" << endl;
+    for (int i = 0; i < Size; i++) {
+        cout << i + 1 << " ";
+        for (int j = 0; j < Size; j++) {
+            cout << board[i][j];
+            if (j < Size - 1) cout << " |";
+        }
+        cout << endl;
+        if (i < Size - 1) cout << "  --+--+--" << endl;
+    }
+    cout << endl;
+}
+
+void TicTacToe::Player() {
+    if (player == 'X') {
+        player = 'O';
+    }
+    else {
+        player = 'X';
+    }
+}
+
+bool TicTacToe::Move(char col, int row) {
+    int c = col - 'A';
+    int r = row - 1;
+    if (c < 0 || c >= Size || r < 0 || r >= Size || board[r][c] == 'X' || board[r][c] == 'O') {
+        cout << "This place is taken" << endl;
+        return false;
+    }
+    board[r][c] = player;
+    return true;
+}
+
+bool TicTacToe::Draw() {
+    for (int i = 0; i < Size; i++) {
+        for (int j = 0; j < Size; j++) {
+            if (board[i][j] != 'X' && board[i][j] != 'O') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
+
---
+++ TictactoeAlina.h
@@ -0,0 +1,9 @@
+#pragma once
+class TicTacToe
+{
+public:
+	void Board();
+	void Player();
+	bool Move(char,int);
+	bool Draw();
+};
