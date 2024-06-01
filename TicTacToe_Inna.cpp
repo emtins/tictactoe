cba7c01e74ba9130f0f75886f5b578d11bac92a5
InnetaSh <inneta3@gmail.com>
2024-06-01 18:11:25 +0300

    Add function Game

--- TicTacToe_Inna.cpp
+++ TicTacToe_Inna.cpp
@@ -39,3 +39,47 @@ bool PlayerMove(int playerNumber, char mark)
     Board();
     return WinLogic(mark);
 }
+
+void Game()
+{
+    cout << "Enter symvol X\\O:\n";
+    cout << "1 player:\t";
+    char player1;
+    player1 = getchar();
+    player1 = toupper(player1);
+    char player2 = ' ';
+
+    if (player1 == 'X')
+    {
+        cout << "2 player: \tO";
+        player2 = 'O';
+    }
+    else
+    {
+        cout << "2 player: \tX";
+        player2 = 'X';
+    }
+    cout << "\n";
+    string winner = "";
+    while (true)
+    {
+        if (PlayerMove(1, player1))
+        {
+            winner = "Player 1";
+            break;
+        }
+
+        if (PlayerMove(2, player2))
+        {
+            winner = "Player 2";
+            break;
+        }
+        if (Draw())
+        {
+            cout << "Draw!\n";
+            break;
+        }
+    }
+    if (winner != "")
+        cout << "Congratulations! Winner: " << winner << "!!!\n";
+}
