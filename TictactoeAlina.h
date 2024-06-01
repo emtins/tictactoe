36b73d49f0cb70570860d1be2e9b2d48bff11b1f
emtins <141028370+emtins@users.noreply.github.com>
2024-06-01 18:46:52 +0300

    Update TictactoeAlina.h

--- TictactoeAlina.h
+++ TictactoeAlina.h
@@ -1,9 +1,5 @@
 #pragma once
 
-const int Size = 3;
-char board[Size][Size] = { {' ',' ',' '}, {' ',' ',' '}, {' ',' ',' '} };
-char player = 'X';
-
 	void Board();
 	void Player();
 	bool Move(char,int);
